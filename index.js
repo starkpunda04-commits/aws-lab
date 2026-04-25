af9bbf84bd7922c297739bdee3bd96e9a8a23916
starkpunda04-commits <starkpunda04@gmail.com>
2026-04-26 03:56:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("hii")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head><title>My App</title></head>
+      <body>
+        <h1>Hello from Node.js 🚀</h1>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(4000, () => console.log("Server running"));
